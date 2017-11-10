fdc2fc11119eebd281406a1a66dfe1d0aadadcd3
Armin Reiter <armin.reiter@live.at>
2017-11-10 21:54:03 +0100

    add and use converter

---
+++ CodingDojo3/CodingDojo3/Converter/StringToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CodingDojo3.Converter
+{
+    public class StringToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ("enabled".Equals(value as string, StringComparison.InvariantCultureIgnoreCase))
+                return new SolidColorBrush(Colors.Green);
+
+            return new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
